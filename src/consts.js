204ed62c6741d38ec41502ca3dd56f29b46e0808
houbingzhi <houbingzhi123@gmail.com>
2023-07-12 20:43:21 +0800

    修改网站细节信息

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_TITLE = `Echo Hou's Blog`;
+export const SITE_DESCRIPTION = 'Along the way, thanks to Feifei - my academic tutor';
+export const SITE_EMAIL = 'houbingzhi123@gmail.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
